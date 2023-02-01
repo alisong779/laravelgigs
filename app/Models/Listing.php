d43eb8daa5bb0910f359a934fd7fc8f4c0e1bce0
Alison Golden <alisong779@yahoo.com>
2023-02-01 10:34:47 -0500

    Create Listing model, ListingFactory and data file

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Listing One',
-                'description' => 'Nullam pulvinar et nibh ut feugiat. Aliquam erat volutpat. Ut non turpis id quam pharetra lobortis.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Listing Two',
-                'description' => 'Pellentesque eu magna egestas, aliquet nibh vel, molestie felis. Nullam suscipit dapibus consectetur. Pellentesque quis tortor dui. Nunc vulputate mattis nulla, nec sollicitudin elit.'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
