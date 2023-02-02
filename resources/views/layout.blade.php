66b666a203409e667d0a389cb4cb07eca8b84824
Alison Golden <alisong779@yahoo.com>
2023-02-02 12:21:19 -0500

    add html, partials, components, set dta

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -28,36 +28,26 @@
     </head>
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
-            <a href="index.html"
-                ><img class="w-24" src="images/logo.png" alt="" class="logo"
-            /></a>
+            <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
                     <a href="register.html" class="hover:text-laravel"
-                        ><i class="fa-solid fa-user-plus"></i> Register</a
-                    >
+                        ><i class="fa-solid fa-user-plus"></i> Register</a>
                 </li>
                 <li>
                     <a href="login.html" class="hover:text-laravel"
                         ><i class="fa-solid fa-arrow-right-to-bracket"></i>
-                        Login</a
-                    >
+                        Login</a>
                 </li>
             </ul>
         </nav>
         <main>
     @yield('content')
         </main>
-        <footer
-        class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
-    >
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+        <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
+            <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
 
-        <a
-            href="create.html"
-            class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-            >Post Job</a
-        >
-    </footer>
-</body>
+            <a href="create.html" class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">Post Job</a>
+        </footer>
+    </body>
 </html>
