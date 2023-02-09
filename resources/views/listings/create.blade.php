4ea2b599e250b1c8717f9c43a9479f29257e745b
Alison Golden <alisong779@yahoo.com>
2023-02-09 13:06:39 -0500

    add file upload, storage, display in views

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -4,7 +4,7 @@
             <h2 class="text-2xl font-bold uppercase mb-1">Create a Gig</h2>
             <p class="mb-4">Post a gig to find a developer</p>
         </header>
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings" enctype="multipart/form-data">
             @csrf
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
@@ -64,6 +64,17 @@
                 @enderror
             </div>
 
+            <div class="mb-6">
+                <label for="logo" class="inline-block text-lg mb-2">
+                    Company Logo
+                </label>
+                <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo"/>
+
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
+            
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">
                     Job Description
