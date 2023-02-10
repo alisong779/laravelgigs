91cd3533e377b0789d3d63a983e49e98b04801d8
Alison Golden <alisong779@yahoo.com>
2023-02-10 14:55:02 -0500

    add hasMany, belongsTo relationships

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -24,4 +24,10 @@ class Listing extends Model
                 ->orWhere('tags', 'like', '%' . request('search') . '%');
         }
     }
+
+    //Relationship to User
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
