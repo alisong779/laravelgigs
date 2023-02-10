403281ffcd4bbd4be286f59fc4bd1ffeed40fdf4
Alison Golden <alisong779@yahoo.com>
2023-02-10 13:34:25 -0500

    revise layout component for login

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -34,12 +34,27 @@
         <nav class="flex justify-between items-center mb-4">
             <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt="" class="logo"/></a>
             <ul class="flex space-x-6 mr-6 text-lg">
+                @auth
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{ auth()->user()->name }}
+                    </span>
+                </li>
+                <li>
+                    <a href="/listings/manage" class="hover:text-laravel"><i class="fa-solid fa-gear"></i> Manage Listings</a>
+                </li>
+                <li>
+                    <a href="/logout" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Logout</a>
+                </li>
+
+                @else
                 <li>
                     <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i>Register</a>
                 </li>
                 <li>
                     <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>Login</a>
                 </li>
+                @endauth
             </ul>
         </nav>
         <main>
